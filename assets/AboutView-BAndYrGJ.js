5577e052ce37b3f21afc360e948b17ec47bc853e
Jeric Moreno <moreno.jeric1995@gmail.com>
2025-05-19 14:56:28 +0800

    adding dist

--- assets/AboutView-BAndYrGJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,a as s,o as a}from"./index-Cw4x6s4H.js";import"jquery";const n={},r={class:"about"};function c(i,e){return a(),t("div",r,e[0]||(e[0]=[s("h1",null,"This is an about page",-1)]))}const u=o(n,[["render",c]]);export{u as default};
