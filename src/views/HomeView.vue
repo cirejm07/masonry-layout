743b7cf334d34ff8e4efdd8a2eca33a0007a94b8
Jeric Moreno <moreno.jeric1995@gmail.com>
2025-05-19 15:48:51 +0800

    xcpiozvhasuidfbjwerew

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@ import { onMounted, ref, nextTick } from 'vue'
 import 'lightbox2'
 import { useGalleryStore } from '../stores/gallery'
 // import 'lightbox2/dist/css/lightbox.css'
-// import 'lightbox2/dist/js/lightbox-plus-jquery.min.js'
+import 'lightbox2/dist/js/lightbox-plus-jquery.min.js'
 import Lightbox from 'lightbox2'
 
 // data
